bc53a20227f2fa4815685011b4515d9dea0238e6
evopen <520dhh@gmail.com>
2019-09-04 01:51:31 +0800

    basic functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace GameLauncher
 {
@@ -22,23 +23,51 @@ namespace GameLauncher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string GamePath = @"D:\Dev\Unreal\UnrealWWY\Saved\StagedBuilds\WindowsNoEditor\";
+        private const string GameFolder = @"D:\Dev\Unreal\UnrealWWY\Saved\StagedBuilds\WindowsNoEditor\";
+
+
+        private List<int> _width = new List<int>()
+        {
+            800, 1024, 1280, 1366, 1400, 1440, 1600, 1680, 1920
+        };
+
+        private List<int> _height = new List<int>()
+        {
+            600, 768, 720, 768, 1050, 900, 900, 1050, 1080
+        };
+
+        private string _projectName = "";
+
+
+        private List<string> _resolutions = new List<string>();
 
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < _height.Count; i++)
+            {
+                _resolutions.Add(_width[i] + " x " + _height[i]);
+            }
 
             MapComboBox.ItemsSource = GetMapNames();
             MapComboBox.SelectedIndex = 0;
-            //System.Windows.SystemParameters.PrimaryScreenWidth;
-            //System.Windows.SystemParameters.PrimaryScreenHeight;
+
+            var resX = Screen.PrimaryScreen.Bounds.Width;
+            var resY = Screen.PrimaryScreen.Bounds.Height;
+            ResolutionComboBox.ItemsSource = _resolutions;
+            var index = _width.IndexOf(resX);
+            int nativeResIndex = 0;
+            if (_height[index] == resY)
+            {
+                nativeResIndex = index;
+            }
+
+            ResolutionComboBox.SelectedIndex = nativeResIndex;
         }
 
         private List<string> GetMapNames()
         {
-            string projectName = "";
-
-            string[] fileEntries = Directory.GetDirectories(GamePath);
+            string[] fileEntries = Directory.GetDirectories(GameFolder);
             foreach (string fileName in fileEntries)
             {
                 string[] subFileEntries = Directory.GetFiles(fileName);
@@ -46,11 +75,12 @@ namespace GameLauncher
                 {
                     if (subFileName.Contains(@".uproject"))
                     {
-                        projectName = System.IO.Path.GetFileNameWithoutExtension(subFileName);
+                        _projectName = System.IO.Path.GetFileNameWithoutExtension(subFileName);
                     }
                 }
             }
-            string[] mapFiles = Directory.GetFiles(GamePath + projectName + @"\Content\Maps\");
+
+            string[] mapFiles = Directory.GetFiles(GameFolder + _projectName + @"\Content\Maps\");
 
             List<string> mapNames = new List<string>();
             foreach (string mapFile in mapFiles)
@@ -60,9 +90,39 @@ namespace GameLauncher
                     mapNames.Add(System.IO.Path.GetFileNameWithoutExtension(mapFile));
                 }
             }
+
             foreach (string mapName in mapNames)
                 Debug.WriteLine(mapName);
             return mapNames;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int parameterCount = 5;
+            int resolutionIndex = ResolutionComboBox.SelectedIndex;
+            int resHeight = _height[resolutionIndex];
+            int resWidth = _width[resolutionIndex];
+            bool? vsync = VSyncCheckBox.IsChecked;
+            bool? fullscreen = FullscreenCheckBox.IsChecked;
+            string mapName = MapComboBox.Text.Trim();
+
+            List<string> parametersList = new List<string>();
+            parametersList.Add(mapName);
+            parametersList.Add("ResX=" + resWidth);
+            parametersList.Add("ResY=" + resHeight);
+            parametersList.Add((bool) vsync ? "-VSync" : "-NoVSync");
+            parametersList.Add((bool) fullscreen ? "-FULLSCREEN" : "-WINDOWED");
+            
+
+            string parameters = "";
+            for (int i = 0; i < parameterCount; i++)
+            {
+                parameters = String.Join(" ", parametersList);
+            }
+
+            string gameEXE = GameFolder + _projectName + @".exe";
+            Debug.WriteLine(parameters);
+            Process.Start(gameEXE, parameters);
         }
     }
 }
